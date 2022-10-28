5aa9288d3d79c459a5a0cbffb72d3e1eb1358a8b
IlnazKamalov <kamalovpk@gmail.com>
2022-10-28 15:55:05 +0300

    fix BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema {
+public class BaseSchema {
 
     private final List<Predicate<Object>> validList = new ArrayList<>();
 
-    protected void addPredicate(Predicate<Object> predicate) {
+    public final void addPredicate(Predicate<Object> predicate) {
         validList.add(predicate);
     }
 
-    public boolean isValid(Object o) {
+    public final boolean isValid(Object o) {
         for (Predicate<Object> objectPredicate: validList) {
             if(!objectPredicate.test(o)) {
                 return false;
